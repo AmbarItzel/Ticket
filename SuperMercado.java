77e37052a1119e32505e4614ddc01d1507d78204
AmbarItzel <ambarcruz898@gmail.com>
2025-11-05 13:34:03 -0600

    Add files via upload

---
+++ SuperMercado.java
@@ -0,0 +1,47 @@
+package com.mx.SuperMercado;
+
+public class SuperMercado {
+	
+	//Atributos
+	private String productos;
+	private double precio;
+	private int stock;
+	
+	//Constructor
+	public SuperMercado(String productos, double precio, int stock) {
+		this.productos = productos;
+		this.precio = precio;
+		this.stock = stock;
+	}
+	//constructor vacio
+	public SuperMercado() {
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "SuperMercado [productos=" + productos + ", precio=" + precio + 
+				", stock=" + stock + "]\n";
+	}
+	
+	//get and set
+	public String getProductos() {
+		return productos;
+	}
+	public void setProductos(String productos) {
+		this.productos = productos;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	
+}
